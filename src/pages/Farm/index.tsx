f661a932729a4e7782783fb4dbe2523a200c65ae
RudeWolfCoding <51991885+RudeWolfCoding@users.noreply.github.com>
2021-04-22 10:50:37 +0200

    Fixed http iframe request

--- src/pages/Farm/index.tsx
+++ src/pages/Farm/index.tsx
@@ -5,7 +5,7 @@ export default function Pool() {
 
   return (
       <AppBody>
-      <iframe src="http://rewards.fuse.io" width="100%" height="750px" scrolling="yes"></iframe>
+      <iframe src="https://rewards.fuse.io" width="100%" height="750px" scrolling="yes"></iframe>
       </AppBody>
 
   )
